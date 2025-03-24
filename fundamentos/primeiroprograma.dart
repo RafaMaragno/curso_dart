2211bdcc5edf6484dee97aadacf9f8ec16422bde
RafaMaragno <rafa.maragno@gmail.com>
2025-03-23 23:50:04 -0300

    Commit - Curso Dart

---
+++ fundamentos/primeiroprograma.dart
@@ -0,0 +1,11 @@
+main() {
+  print('Início do programa Dart');
+  bool fimDoPrograma = false;
+
+  {
+    fimDoPrograma = true;
+    if(fimDoPrograma) {
+      print('Fim do programa Dart');
+    }
+  }
+}
