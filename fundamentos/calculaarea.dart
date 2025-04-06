88c52f62a2488e7bf5dfd2be1924b178c88879eb
RafaMaragno <rafa.maragno@gmail.com>
2025-04-06 20:43:41 -0300

    Fundamentos - Dart

--- fundamentos/calculaarea.dart
+++ fundamentos/calculaarea.dart
@@ -3,8 +3,11 @@ import 'dart:io';
 
 main() {
 
-  //Uma variável do tipo 'final' não pode ser alterada
-  final PI = 3.1415;
+  //Variáveis dos tipos 'const' e 'final' não podem ser alteradas
+  // 'const' - Valor é definido em tempo de compilação;
+  // 'final' - Valor é definido em tempo de execução do programa;  
+
+  const PI = 3.1415;
 
   //O comando 'stdout.write' é utilizado para solicitar uma entrada do usuário
   stdout.write("Informe o valor do raio da circunferência: ");
@@ -12,8 +15,10 @@ main() {
   //O comando 'stdin.readLineSync()' é utilizado para receber uma entrada do usuário
   final entrada = stdin.readLineSync();
 
+  //Conversão da entrada do usário em tipo 'double'
   final raio = double.parse(entrada!);
   
+  //Cálculo da área da circunferência
   final areaDaCircunferencia = PI * raio * raio;
   
   print("O valor da área da circunferência é: " + areaDaCircunferencia.toString());
