d2c2cd5fa8b942b6a3919841eed74bee434a2e9f
RafaMaragno <rafa.maragno@gmail.com>
2025-04-21 18:51:18 -0300

    Fundamentos - Dart

--- fundamentos/operadores.dart
+++ fundamentos/operadores.dart
@@ -1,5 +1,7 @@
 main() {
-  /* Aritméticos - Operadores binários */
+  
+  //Operadores Aritméticos - Operadores binários
+
   int n1 = 8;
   int n2 = 4;
   
@@ -15,7 +17,8 @@ main() {
         ' / Divisão: ' + divi.toString() + 
         ' / Resto: ' + rest.toString());
 
-  /* Operadores lógicos */
+  //Operadores lógicos
+  
   bool produtoFragil = true;
   bool produtoCaro = false;
 
@@ -25,7 +28,7 @@ main() {
 
   // Operador lógico || - 'OU'
   print('O produto é frágil ou caro? ');
-  print(produtoFragil || produtoCaro);
+  print(produtoCaro || produtoFragil);
 
   // Operador lógico ^ - 'OU EXCLUSIVO' - 'True' se somente uma das condições for verdadeira 
   print('O produto é somente frágil ou somente caro? ');
@@ -34,5 +37,33 @@ main() {
   // Operador lógico ! - 'Não/Not - NEGAÇÃO LÓGICA' - Inverte o valor atual
   print('O produto é frágil? ');
   print(!produtoFragil);
+  
+  // Operador lógico & - 'Operação bit a bit'
+  print (4 & 5);
+  //bate bit a bit, o que for igual mantém, o que for diferente mantém 0;
+  //4 - 100
+  //5 - 101
+  //=   100 - Ou seja (4 & 5) ==> 4
 
-}
+  //Operadores de atribuição
+
+  double num1 = 1;
+  num1 += 2; // 'num1 += 2' Adicionando 2 à variável num1 = 1+2 = 3
+  print(num1);
+  num1 -= 1; // 'num1 -= 1' Subtraindo 1 da variável num1 = 3-1 = 2
+  print(num1); 
+  num1 *= 2; // 'num1 *= 2' Multiplicando a variável num1 por 2 = 2*2 = 4
+  print(num1);
+  num1 /= 2; // 'num1 /= 2' Dividindo a variável num1 por 2 = 4/2 = 2
+  print(num1);
+
+  //Operadores relacionais - O resultado sempre é um boolean
+
+  print(1 < 2);   //--> 1 é menor que 2? => TRUE
+  print(1 <= 2);  //--> 1 é menor ou igual à 2? => TRUE
+  print(1 > 2);   //--> 1 é maior que 2? => FALSE
+  print(1 >= 2);  //--> 1 é maior ou igual à 2? => FALSE
+  print(1 != 2);  //--> 1 é não igual à 2? => TRUE
+  print(1 == 2);  //--> 1 é igual à 2? => FALSE
+
+  }
