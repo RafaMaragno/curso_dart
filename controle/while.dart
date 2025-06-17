82e10eadefc81d259985f887468883df964b6710
RafaMaragno <rafa.maragno@gmail.com>
2025-06-16 23:33:23 -0300

    [16/06/2025]Estruturas de controle (while).dart

---
+++ controle/while.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+main() {
+  var input = ' ';
+
+  //While - Executa o bloco enquanto o valor for diferente de 'sair' (!=)
+  while(input != 'sair') {
+    stdout.write('Digite algo ou digite sair: ');
+    input = stdin.readLineSync().toString();
+  }
+
+  print('Fim!');
+
+  //Do while - Quando é o exato o "do", o bloco é executado pelo menos uma vez.
+  do {
+    stdout.write('Digite algo ou digite sair: ');
+    input = stdin.readLineSync().toString();
+  } while(input != 'sair');
+}
