4abbf2e3c2cd0152d4a9356546c8eb4073d24c5a
RafaMaragno <rafa.maragno@gmail.com>
2025-06-20 17:36:39 -0300

    [20/06/2025]Funcoes.dart

--- funcoes/funcao.dart
+++ funcoes/funcao.dart
@@ -2,11 +2,16 @@ import 'dart:math';
 
 main() {
 
+//Funções sem retorno (void)
   somaInteiros(1,2);
-  somaInteiros(3,4);
-
   somaNumerosAleatorios();
 
+//Funções com retorno
+  int resultado = funcaoSoma(3, 4);
+  print('O resultado da soma é $resultado');
+  
+  int somaAleatoria = funcaoSomaAleatoria();
+  print('O resultado da soma aleatória é $somaAleatoria');
 }
 
 //Void - Significa que a função não retorna nada
@@ -18,4 +23,15 @@ void somaNumerosAleatorios() {
   int n1 = Random().nextInt(11);
   int n2 = Random().nextInt(11);
   print('A soma de $n1 e $n2 é ${n1 + n2}');
+}
+
+//Fução com retorno (Tem que especificar o tipo do retorno)
+int funcaoSoma(int a, int b) {
+  return(a + b);
+}
+
+int funcaoSomaAleatoria() {
+  int n1 = Random().nextInt(11);
+  int n2 = Random().nextInt(11);
+  return n1 + n2;
 }
