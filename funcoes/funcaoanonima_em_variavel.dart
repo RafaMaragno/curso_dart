17dc09343782a7cc8cc04990c51bd1c3a33b8683
RafaMaragno <rafa.maragno@gmail.com>
2025-07-02 22:20:16 -0300

    [02/07/2025]Função em variável.dart

--- funcoes/funcaoanonima_em_variavel.dart
+++ funcoes/funcaoanonima_em_variavel.dart
@@ -1,11 +1,21 @@
+import 'dart:math';
+
 main () {
   var adicao = (int a, int b) {
     return a + b;
   };
 
-  //Função anônima usando Arrow "=>"
+  //Funções anônimas usando Arrow "=>"
   var subtracao = (int a, int b) => a - b;
+  var multiplicacao = (int a, int b) => a * b;
+  var divisao = (int a, int b) => a / b;
 
-  print('Soma: ${adicao(5, 5)}');
-  print('Subtração: ${subtracao(5, 5)}');
+  int n1;
+  int n2;
+  print('Os números são: ${n1 = Random().nextInt(10)} e ${n2 = Random().nextInt(10)}');
+  print('Soma: ${adicao(n1, n2)}');
+  print('Subtração: ${subtracao(n1, n2)}');
+  print('Multiplicação: ${multiplicacao(n1, n2)}');
+  print('Divisão: ${divisao(n1, n2)}');
+
 }
