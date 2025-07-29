c7f33f88de3f82525819546963bae0ae45f12641
RafaMaragno <rafa.maragno@gmail.com>
2025-07-28 21:26:17 -0300

    [28/07/2025]Filter.dart

--- funcoes/filter01.dart
+++ funcoes/filter01.dart
@@ -1,11 +1,23 @@
 main() {
+  //+++
+  //Forma 1 de "filtrar" valores dentro de uma lista
+  //+++
   var notas = [10.0, 9.5, 6.0, 10.0, 5.5, 8.0, 5.0, 7.5, 8.5];
-  var notasAprovadas = [];
+  var notasAltas = [];
 
   for (var nota in notas) {
     if (nota >= 7.0) {
-      notasAprovadas.add(nota);
+      notasAltas.add(nota);
     }
   }
-  print(notasAprovadas);
+  print(notasAltas);
+
+  //+++
+  //Forma 2 de "filtrar" valores dentro de uma lista
+  //+++
+  bool Function(double) notasAltasFunction = (double nota) => nota >= 7.0;
+
+  var notasAltasFiltradas = notas.where(notasAltasFunction);
+  print(notasAltasFiltradas);
+  
 }
